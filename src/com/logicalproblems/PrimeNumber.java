868a86d131923fba1e465ea4de9f82b4ea947104
erakshaykumar <akaakshay304@gmail.com>
2022-02-10 07:59:46 +0530

    added: reverse of number

--- src/com/logicalproblems/PrimeNumber.java
+++ src/com/logicalproblems/PrimeNumber.java
@@ -5,24 +5,24 @@ import java.util.Scanner;
 public class PrimeNumber {
 	public static void checkPrime() {
 		int i, count=0;
-		Scanner sc = new Scanner(System.in);
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("Enter a Number: ");
+			int num = sc.nextInt();
 
-		System.out.print("Enter a Number: ");
-		int num = sc.nextInt();
-
-		for(i = 2; i <= num/2 ; i++)
-		{
-			if(num % i == 0)
+			for(i = 2; i <= num/2 ; i++)
 			{
-				count++;
-				break;
+				if(num % i == 0)
+				{
+					count++;
+					break;
+				}
 			}
-		}
 
-		if(count==0 && num!=1)
-			System.out.println("\nIt is a Prime Number. ");
-		else
-			System.out.println("\nIt is not a Prime Number. ");
+			if(count==0 && num!=1)
+				System.out.println("\nIt is a Prime Number. ");
+			else
+				System.out.println("\nIt is not a Prime Number. ");
+		}
 	}
 
 
--- src/com/logicalproblems/StopWatch.java
+++ src/com/logicalproblems/StopWatch.java
@@ -8,15 +8,15 @@ public class StopWatch {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Press 1 to start the stop watch");
 		double startWatch=sc.nextDouble();
-		
+
 		startWatch=System.currentTimeMillis();
-		
+
 		System.out.println("Press 0 to stop the stop watch");
 		double stopwatch=sc.nextDouble();
 		stopwatch=System.currentTimeMillis();
-		
+
 		System.out.println("The Elapsed time is  "+((stopwatch-startWatch)/1000) + " seconds ");
-		
+
 		sc.close();
 
 	}
